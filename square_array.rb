2929a5125bb3c0a4a58459ae094fe59d13180d82
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-12 15:20:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,3 @@
 def square_array(array)
-    apples_in_basket = array.size # Step 1
-    apples_taken_out = 0 # Step 2
-   
-  # Step 3 + 4
-  while apples_taken_out < apples_in_basket
-      print apples_taken_out ** apples_taken_out
-      apples_taken_out += 1
-  end
+    
 end
